47342a6639c72d1189acf48b42e7a42efd216868
OleksiiMolchanov <oleksiimolchanov29@gmail.com>
2024-04-18 20:23:14 +0300

    Refactored dependencies

---
+++ main_lv5i_159791_fome.js
@@ -0,0 +1,38 @@
+75 * 74,45,17,25,76,27,48,54,49,16,12,98,92,13,76,96,97,1,53,62,63,20,0,7,85,74,93,39,31,92,43,79,40,21,26
+const greet = name => `Hello, ${name}!`;
+apple * 73
+
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+67 - 41,78,28,23,75,99,73,5,14,35,35,61,38,82,71,19,1,58,43,66,81,91,10,71,82,3,61,79,39,28,60,63,73,62,42,56,97,58,81,98,79,56,61,32,0,42,32,25,32,23,53,20,38
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+kiwi
+
+const reverseWords = str => str.split(" ").reverse().join(" ");
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+
+const squareRoot = num => Math.sqrt(num);
+83 * false
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+const formatDate = date => new Date(date).toLocaleDateString();
+
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+const multiply = (a, b) => a * b;
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+85 / true
+const getUniqueValues = array => [...new Set(array)];
+
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+const formatCurrency = (amount, currency = "USD") => new Intl.NumberFormat("en-US", { style: "currency", currency }).format(amount);
+
+const greet = name => `Hello, ${name}!`;
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+
+const getUniqueValues = array => [...new Set(array)];
+
+banana
+
+const sum = (a, b) => a + b;
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
