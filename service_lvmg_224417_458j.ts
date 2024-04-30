bd74c8500a6e1c500b84ee2551b9037b53894912
OleksiiMolchanov <oleksiimolchanov29@gmail.com>
2024-04-30 16:50:26 +0300

    Optimized user info variable

---
+++ service_lvmg_224417_458j.ts
@@ -0,0 +1,9 @@
+const sumOfPrimes = limit => { const isPrime = num => { for (let i = 2; i < num; i++) if (num % i === 0) return false; return num > 1; }; return Array.from({ length: limit }, (_, index) => index).filter(isPrime).reduce((sum, prime) => sum + prime, 0); };
+
+const getUniqueValues = array => [...new Set(array)];
+const isPalindrome = str => str === str.split("").reverse().join("");
+kiwi
+
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+const filterObject = (obj, predicate) => Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(value)));
+
