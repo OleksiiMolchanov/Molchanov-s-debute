f2a8cd028e17ea9d1a56dcd20eef02f7280f5770
OleksiiMolchanov <oleksiimolchanov29@gmail.com>
2024-05-02 22:57:04 +0300

    Optimized tests

---
+++ main_lvpo_243144_3boa.js
@@ -0,0 +1,5 @@
+const fetchData = async url => { const response = await fetch(url); return response.json(); }
+const randomNumber = getRandomNumber();
+banana
+
+const getRandomSubset = (array, size) => array.slice(0, size);
